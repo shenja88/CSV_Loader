4319e99192a331d4dd6f8c64103db1a335175fa5
shenja88 <voluevich88@gmail.com>
2022-05-02 11:52:15 +0300

    init commit

---
+++ src/main/java/by/voluevich/testjob_inventolabs3/TestJobInventoLabs3Application.java
@@ -0,0 +1,13 @@
+package by.voluevich.testjob_inventolabs3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJobInventoLabs3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJobInventoLabs3Application.class, args);
+    }
+
+}
---
+++ src/test/java/by/voluevich/testjob_inventolabs3/TestJobInventoLabs3ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.voluevich.testjob_inventolabs3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJobInventoLabs3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
